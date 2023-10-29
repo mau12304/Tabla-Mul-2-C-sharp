ba638049ba7744cd1e53068c25d0ec0222765616
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-29 09:35:12 -0600

    Agregar archivos de proyecto.

---
+++ Tabla de Multiplicar 2 C#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+
+namespace TABLAS_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, RESUL, T, I;
+
+            string linea;
+
+            Console.Write("CUANTAS TABLAS: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+
+            T = 1;
+            while (T <= NUM)
+            {
+                I = 10;
+                while (I >= 1)
+                {
+                    RESUL = T * I;
+                    Console.WriteLine("{0}*{1}={2}", T, I, RESUL);
+                    I = I - 1;
+                }
+                Console.Write("Pulse una Tecla:");
+                Console.ReadLine();
+                T = T + 1;
+            }
+        }
+    }
+}
